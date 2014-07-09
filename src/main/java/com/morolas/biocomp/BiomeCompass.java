7c0844645f63e81ca5e3def779e2742572a0ae9a
JPoffyn <jonas.poffyn@skynet.be>
2014-07-09 14:11:39 +0200

    Setup Environment + Mod Class file
    End of episode 3

---
+++ src/main/java/com/morolas/biocomp/BiomeCompass.java
@@ -0,0 +1,26 @@
+package com.morolas.biocomp;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="biocomp", name="Biome Compass", version="1.7.10-0.1")
+
+public class BiomeCompass {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
+
