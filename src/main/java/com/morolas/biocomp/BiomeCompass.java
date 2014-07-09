3c912fb006d8ef20df4573304106a8abd28478db
JPoffyn <jonas.poffyn@skynet.be>
2014-07-09 14:44:08 +0200

    Revert "Revert "End episode 6""
    This reverts commit 1391f7d31472a979fd8caf25bfb3d73b0c5fdba0.
    
    Conflicts:
    	BiomeCompass.iws

--- src/main/java/com/morolas/biocomp/BiomeCompass.java
+++ src/main/java/com/morolas/biocomp/BiomeCompass.java
@@ -8,6 +8,10 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid="biocomp", name="Biome Compass", version="1.7.10-0.1")
 
 public class BiomeCompass {
+
+    @Mod.Instance("biocomp")
+    public static BiomeCompass instance;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
 
