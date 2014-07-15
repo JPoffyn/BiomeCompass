a6e9583c7c0fef75411d02cd51c977e47f8697d8
JPoffyn <jonas.poffyn@skynet.be>
2014-07-15 20:05:56 +0200

    End episode 7
    new config and logger

--- src/main/java/com/morolas/biocomp/BiomeCompass.java
+++ src/main/java/com/morolas/biocomp/BiomeCompass.java
@@ -1,14 +1,15 @@
 package com.morolas.biocomp;
 
-import com.morolas.biocomp.configuration.ConfigurationHandler;
+import com.morolas.biocomp.handler.ConfigurationHandler;
 import com.morolas.biocomp.proxy.IProxy;
 import com.morolas.biocomp.reference.Reference;
+import com.morolas.biocomp.utility.LogHelper;
 import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 
 public class BiomeCompass
 {
@@ -22,18 +23,20 @@ public class BiomeCompass
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialisation Complete!");
     }
 }
 
--- src/main/java/com/morolas/biocomp/reference/Reference.java
+++ src/main/java/com/morolas/biocomp/reference/Reference.java
@@ -9,5 +9,6 @@ public class Reference
     public static final String VERSION = "1.7.10-0.1";
     public static final String CLIENT_PROXY_CLASS = "com.morolas.biocomp.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.morolas.biocomp.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.morolas.biocomp.client.gui.GuiFactory";
 
 }
